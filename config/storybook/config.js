09f3cafdb46d1b0e1276498ea4d89a843883c49e
Víctor <victormonserratvillatoro@gmail.com>
2018-08-06 23:49:13 +0200

    Adding some Storybook addons

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,4 +1,9 @@
-import { configure } from '@storybook/react';
+import centered from '@storybook/addon-centered';
+import { withKnobs } from '@storybook/addon-knobs/react';
+import { configure, addDecorator } from '@storybook/react';
+
+addDecorator(centered);
+addDecorator(withKnobs);
 
 loadStories = () => {
     // You can require as many stories as you need.
