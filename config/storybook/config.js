12dac8e6af91bbf719d3bc84f34e7eade3c05d29
Víctor <victormonserratvillatoro@gmail.com>
2018-08-07 17:09:29 +0200

    Setting up Storybook Semantic UI CSS custom head tag

--- config/storybook/config.js
+++ config/storybook/config.js
@@ -1,7 +1,6 @@
 import centered from '@storybook/addon-centered';
 import { withKnobs } from '@storybook/addon-knobs/react';
 import { configure, addDecorator } from '@storybook/react';
-import 'semantic-ui-css/semantic.min.css';
 
 addDecorator(centered);
 addDecorator(withKnobs);
