664b250295af5242cc8ea55ae0ef00a6c50a99c5
Víctor <victormonserratvillatoro@gmail.com>
2018-08-08 21:10:39 +0200

    Total pages type definition at idea pagination

--- src/Application/Idea/stories/IdeasPagination.tsx
+++ src/Application/Idea/stories/IdeasPagination.tsx
@@ -1,24 +1,17 @@
-import * as React from 'react';
-
 import { action } from '@storybook/addon-actions';
 import { number } from '@storybook/addon-knobs/react';
 import { storiesOf } from '@storybook/react';
+import * as React from 'react';
 
 import IdeasPagination from '../components/IdeasPagination';
 
-const defaultTotalPages = 10;
-const minTotalPages = 2;
+const totalPages: number = 10;
 
 storiesOf('Ideas pagination', module)
     .add('default', () => (
         <IdeasPagination
             handleOnPageChange={action('page-changed')}
-            totalPages={number('Páginas', defaultTotalPages, {
-                max: Infinity,
-                min: minTotalPages,
-                range: true,
-                step: 1
-            })}
+            totalPages={number('Total pages', totalPages, {max: Infinity, min: 2, range: true, step: 1})}
         />
     ))
 ;
