a06203312af045a45ab603ae4cb4177ffb23a070
Víctor <victormonserratvillatoro@gmail.com>
2018-08-09 17:02:00 +0200

    Created idea card title styled component

---
+++ src/Application/Idea/components/IdeaCard/IdeaCardTitle.tsx
@@ -0,0 +1,7 @@
+import styled from '../../../../../config/styled-components/styled-components';
+
+import { Card } from 'semantic-ui-react';
+
+export default styled(Card.Header)`
+  padding-top: 0.5em;
+`;
