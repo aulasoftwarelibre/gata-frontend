5f40b2c0df009b3fcd8ca0edd69fd305b5e73ecd
Víctor <victormonserratvillatoro@gmail.com>
2018-08-09 17:02:56 +0200

    Exposing some idea status constants

--- src/Domain/Idea/IdeaStatus.tsx
+++ src/Domain/Idea/IdeaStatus.tsx
@@ -1,2 +1,8 @@
-type IdeaStatus = 'accepted' | 'pending' | 'rejected';
+export type IdeaStatus = 'accepted' | 'pending' | 'rejected';
+
+export const accepted: IdeaStatus = 'accepted';
+export const pending: IdeaStatus = 'pending';
+export const rejected: IdeaStatus = 'rejected';
+export const values: IdeaStatus[] = [accepted, pending, rejected];
+
 export default IdeaStatus;
