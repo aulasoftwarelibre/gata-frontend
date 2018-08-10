5abf9c48d59f3ea24174992f998c3d402087c072
Víctor <victormonserratvillatoro@gmail.com>
2018-08-10 10:04:03 +0200

    Randomized pagination total pages

--- src/Application/Idea/stories/IdeasPagination.tsx
+++ src/Application/Idea/stories/IdeasPagination.tsx
@@ -1,17 +1,18 @@
 import { action } from '@storybook/addon-actions';
 import { number } from '@storybook/addon-knobs/react';
 import { storiesOf } from '@storybook/react';
+import * as faker from 'faker';
 import * as React from 'react';
 
 import IdeasPagination from '../components/IdeasPagination';
 
-const totalPages: number = 10;
+const totalPages: number = faker.random.number({max: 100, min: 2});
 
 storiesOf('Ideas pagination', module)
     .add('default', () => (
         <IdeasPagination
             handleOnPageChange={action('page-changed')}
-            totalPages={number('Total pages', totalPages, {max: Infinity, min: 2, range: true, step: 1})}
+            totalPages={number('Total pages', totalPages, {max: 100, min: 2, range: true, step: 1})}
         />
     ))
 ;
