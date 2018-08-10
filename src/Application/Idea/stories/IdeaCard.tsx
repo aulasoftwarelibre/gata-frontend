480d6dbe07013f5aad36fe3275ef8efc61ab30ae
Víctor <victormonserratvillatoro@gmail.com>
2018-08-10 10:09:00 +0200

    Attendees & comments number customizable params in story

--- src/Application/Idea/stories/IdeaCard.tsx
+++ src/Application/Idea/stories/IdeaCard.tsx
@@ -11,36 +11,40 @@ import User from 'src/Domain/User/User';
 import IdeaCard from '../components/IdeaCard/IdeaCard';
 
 const attendee: boolean = false;
-const attendees: User[] = Array<User>(faker.random.number({max: 10, min: 0})).map(() => {
-    const firstName = faker.name.firstName();
-    const lastName = faker.name.lastName();
+const attendees = (number: number): User[] =>
+    Array<User>(number).map(() => {
+        const firstName = faker.name.firstName();
+        const lastName = faker.name.lastName();
 
-    return({
-        avatar: faker.image.avatar(),
-        firstName: firstName,
-        lastName: lastName,
-        username: faker.internet.userName(firstName, lastName),
-    });
-});
-const author: string = faker.name.findName();
-const closed: boolean = false;
-const comments: Comment[] = Array<Comment>(faker.random.number({max: 10, min: 0})).map(() => {
-    const firstName = faker.name.firstName();
-    const lastName = faker.name.lastName();
-
-    return({
-        author: {
+        return ({
             avatar: faker.image.avatar(),
             firstName: firstName,
             lastName: lastName,
             username: faker.internet.userName(firstName, lastName),
-        },
-        text: faker.lorem.sentences(),
-    });
-});
+        });
+    })
+;
+const author: string = faker.name.findName();
+const closed: boolean = false;
+const comments = (number: number): Comment[] =>
+    Array<Comment>(number).map(() => {
+        const firstName = faker.name.firstName();
+        const lastName = faker.name.lastName();
+
+        return({
+            author: {
+                avatar: faker.image.avatar(),
+                firstName: firstName,
+                lastName: lastName,
+                username: faker.internet.userName(firstName, lastName),
+            },
+            text: faker.lorem.sentences(),
+        });
+    })
+;
 const datetime: Date = faker.date.recent();
 const description: string = faker.lorem.paragraphs();
-const seats: number = faker.random.number({max: 10, min: attendees.length});
+const seats: number = faker.random.number({max: 100, min: attendees.length});
 const status: IdeaStatus = faker.random.arrayElement(statusValues);
 const title: string = faker.lorem.sentence();
 
@@ -48,17 +52,17 @@ storiesOf('Idea card', module)
     .add('default', () => (
         <IdeaCard
             attendee={boolean('Are you attendee?', attendee)}
-            attendees={attendees}
+            attendees={attendees(number('Attendees', 10, {max: 100, min: 0, range: true, step: 1}))}
             author={text('Author', author)}
             closed={boolean('Is closed?', closed)}
-            comments={comments}
+            comments={comments(number('Comments', 10, {max: 100, min: 0, range: true, step: 1}))}
             datetime={date('Date', datetime)}
             description={text('Description', description)}
             handleOnAttendeesClick={action('attendees-clicked')}
             handleOnCommentsClick={action('comments-clicked')}
             handleOnExitClick={action('exit-clicked')}
             handleOnJoinClick={action('join-clicked')}
-            seats={number('Seats', seats, {max: Infinity, min: 1, range: true, step: 1})}
+            seats={number('Seats', seats)}
             status={select('Status', statusValues, status)}
             title={text('Title', title)}
         />
